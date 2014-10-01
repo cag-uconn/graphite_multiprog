aedb01c724b334e0a8b91fa6c91c1f623d6a4389
George Kurian <gkurian@csail.mit.edu>
2014-10-01 15:52:26 -0400

    [condor] Enabled starting multiple simulations simultaneously

--- tools/scheduler/condor_scheduler.py
+++ tools/scheduler/condor_scheduler.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
+import sys
 import time
+import multiprocessing
 
 from scheduler import Scheduler
 from condor_submit_job import CondorSubmitJob
@@ -12,14 +14,33 @@ class CondorScheduler(Scheduler):
       Scheduler.__init__(self, jobs, results_dir, config_filename)
 
    def start(self):
-      output_dir_list = []
-      for job in self.jobs:
-         job.spawn()
-         job.wait()
-         output_dir_list.append(job.output_dir)
+      self.output_dir_list = []
+      self.num_spawned = 0
+      self.num_joined = 0
+      self.num_active = 0
+      self.THRESHOLD = multiprocessing.cpu_count()
+
+      while self.num_joined < len(self.jobs):
+         self.spawnJob()
+         self.waitJob()
+      
       # Submit the Condor job
-      self.condor_job = CondorSubmitJob(self.results_dir, output_dir_list)
+      self.condor_job = CondorSubmitJob(self.results_dir, self.output_dir_list)
       self.condor_job.spawn()
+
+   def spawnJob(self):
+      if self.num_active < self.THRESHOLD and self.num_spawned < len(self.jobs):
+         job = self.jobs[self.num_spawned]
+         job.spawn()
+         self.output_dir_list.append(job.output_dir)
+         self.num_active += 1
+         self.num_spawned += 1
+
+   def waitJob(self):
+      if self.num_active == self.THRESHOLD or self.num_spawned == len(self.jobs):
+         self.jobs[self.num_joined].wait()
+         self.num_active -= 1
+         self.num_joined += 1
 
    def iterate(self):
       # Poll the condor_job to see if it has completed
